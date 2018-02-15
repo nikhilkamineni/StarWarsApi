f236c07c07f01133e4dbd4fba50572a33559885f
Nikhil Kamineni <nikhil@nk-mbp.local>
2018-02-15 17:52:40 -0500

    Setup Router in App.js; need to wire up actual routing

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import styled from 'styled-components';
 import CharList from './components/CharList';
+import CharDetails from './components/CharDetails';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <CharList />
+        <Router>
+          <div>
+            <Route path="/" component={CharList} exact />
+            <Route path="/:id" component={CharDetails} />
+          </div>
+        </Router>
         <div className="Header">REACT WARS</div>
       </div>
     );
--- starwars/src/components/CharDetails.js
+++ starwars/src/components/CharDetails.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
 
+class CharDetails extends Component {
+  render() {
+    return(
+      <h1>CharDetails here</h1>
+    )
+  }
+}
 
 export default CharDetails;
